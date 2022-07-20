c9d6ee315061815767ad77dfb52383e77531a285
Sabina <sanas1982@mail.ru>
2022-07-20 11:21:22 +0300

    добавлено решение задачи4

--- Namber4/Program.cs
+++ Namber4/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("ВВЕДИТЕ ПЕРВОЕ ЧИСЛО");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ВВЕДИТЕ ВТОРОЕ ЧИСЛО");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ВВЕДИТЕ ТРЕТЬЕ ЧИСЛО");
+int number3 = Convert.ToInt32(Console.ReadLine()); 
+
+int num = number1;
+if (number2>num)
+
+{
+    num = number2;
+}
+
+if (number3>num)
+
+{
+    num = number3;
+}
+Console.Write("Максимальное число:");
+Console.Write(num);
