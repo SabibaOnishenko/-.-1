b300d8805909f9e3694943d35d12d701dd8dafb3
Sabina <sanas1982@mail.ru>
2022-07-20 11:48:56 +0300

    добавлено решение задачи 6 дз1

--- Namber6/Program.cs
+++ Namber6/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("ВВЕДИТЕ ЧИСЛО");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number%2==0)
+{
+ Console.WriteLine("Четное число");
+}
+else Console.WriteLine("Нечетное число");
