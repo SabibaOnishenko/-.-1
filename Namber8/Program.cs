26977a137a8ff6a35d4c5938f305014a7f7ad951
Sabina <sanas1982@mail.ru>
2022-07-20 12:37:01 +0300

    добавлно решение задачи 8

--- Namber8/Program.cs
+++ Namber8/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("ВВЕДИТЕ ЧИСЛО");
+int numberX = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count<=numberX)
+{
+    if (count%2==0)
+        {
+            Console.Write(count);
+            Console.Write(", ");
+        }
+        count++;
+}
+
