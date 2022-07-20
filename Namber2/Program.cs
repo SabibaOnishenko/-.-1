1f0aa13f480eed55d4921e303f4025538e29fbb4
Sabina <sanas1982@mail.ru>
2022-07-20 10:58:16 +0300

    добавлена задача2 поиск наибольшего

--- Namber2/Program.cs
+++ Namber2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("ВВЕДИТЕ ПЕРВОЕ ЧИСЛО");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ВВЕДИТЕ ВТОРОЕ ЧИСЛО");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1>number2) 
+{
+    Console.WriteLine(number1);
+}
+else Console.WriteLine(number2);
